34a324ed8e4fa6cfcd1d0ec746f577f99628559f
Meen <meenney.077242239@gmail.com>
2023-09-18 00:30:39 +0700

    add exercise 1-8

--- ex-3.js
+++ ex-3.js
@@ -3,4 +3,6 @@
 let totalFileSize = 250000;
 let uploadedSize = 30000;
 
-// Start coding here
+let uploadProgress = totalFileSize*(100-30000)/100;
+console.log(uploadProgress);
+
--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
-// Start coding here
+let beforePrice = 27000;
+let totalDiscount = beforePrice*(25/100);
+let totalPrice = beforePrice - totalDiscount;
+console.log(totalPrice);
+
